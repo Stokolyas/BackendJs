9cc98a0cf1598d5a94fb3404fcc137796c080dd5
Unknown <alexey.stokolyas@gmail.com>
2018-07-13 15:02:46 +0300

    Mocha

--- project3.js
+++ project3.js
@@ -17,7 +17,7 @@ function catsGroupGenerate(n, gender = 'm') {
     }
     catList[i] = cat.name + ' ' + cat.age + ' ' + cat.gender + ' ' + +cat.legsCount + ' ' + cat.tailLength + ' '
   }
-  return catList//возвращение массива котов
+  return catList //возвращение массива котов
 }
 // возвращение имен котов
 function back_Cat_Name(list) {
@@ -52,7 +52,7 @@ function oldCats(n) {
     }
 
   }
-  return countOldCats>0
+  return countOldCats > 0
 }
 // молодые кошки
 function youngCats(n) {
@@ -61,7 +61,7 @@ function youngCats(n) {
   for (var i = 0; i < catList.length; i++) {
     if (catList[i].indexOf("w") > 0) {
       if (catList[i].substr(catList[i].indexOf("w") - 3, 2) > 5) {
-        nameYoungCats += catList[i].substr(0,5)
+        nameYoungCats += catList[i].substr(0, 5)
       }
 
     }
@@ -72,7 +72,7 @@ function youngCats(n) {
 // счетчик имен
 function nameStats(list) {
   var catName = '',
-    countName = [0,0,0,0]
+    countName = [0, 0, 0, 0]
   for (var i = 0; i < list.length; i++) {
     catName = list[i]
     switch (catName[0]) { //обработка каждого первого символа
@@ -98,9 +98,9 @@ function nameStats(list) {
 class Cat {
 
   constructor(catName, catGender, name, age, gender, legsCount, tailLength) {
-    catName = ['Kail','John','Hell','Gary']
+    catName = ['Kail', 'John', 'Hell', 'Gary']
     this.name = pick(catName)
-    catGender = ['m','w']
+    catGender = ['m', 'w']
     this.age = Math.floor(Math.random(0, 30) * (30 + 1))
     this.gender = pick(catGender)
     this.legsCount = Math.floor(Math.random(1, 4) * (4) + 1)
@@ -110,15 +110,16 @@ class Cat {
 }
 
 // Котофабрика
-function catFactory(Cat){
+function catFactory(Cat) {
+
   cat.gender = 'm'
   return cat.gender
 }
 
 let cat = new Cat() // создание объекта класса
 catFactory() // вызов котофабрики
-var list =[]
-  list = catsGroupGenerate(9) // генерация массива котов
+var list = []
+list = catsGroupGenerate(9) // генерация массива котов
 var NamesCats = nameStats(list) //счет имен котов
 var listName = back_Cat_Name(list) // вывод имен котов
 oldCats(9)
