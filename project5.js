0f70436a06b0606e956c0ed3d0acac8430cb918c
Stokolyas <alexey.stokolyas@gmail.com>
2018-07-17 11:35:19 +0300

    js 5

--- project5.js
+++ project5.js
@@ -1,30 +1,33 @@
 'use strict'
 // счетчик имен
-import {list} from "./project3.js";
+import {
+    list
+} from "./project3.js";
+
 function nameStats(list) {
-    console.log(list)
     var catName = '',
-      countName = [0, 0, 0, 0]
-    for (var i = 0; i < list.length; i++) {
-      catName = list[i]
-      switch (catName[0]) { //обработка каждого первого символа
-        case 'K':
-          ++countName[0]
-          break;
-        case 'J':
-          ++countName[1]
-          break;
-        case 'H':
-          ++countName[2]
-          break;
-        case 'G':
-          ++countName[3]
-          break;
-        default:
-  
-      }
-    }
-    console.log(countName)
+        countName = [0, 0, 0, 0]
+    list.forEach(function(item) {
+        for (const key in item) {
+            switch (item[key]) { //обработка каждого первого символа
+                case 'Kail':
+                    ++countName[0]
+                    break;
+                case 'John':
+                    ++countName[1]
+                    break;
+                case 'Hell':
+                    ++countName[2]
+                    break;
+                case 'Gary':
+                    ++countName[3]
+                    break;
+                default:
+
+            }
+        }
+    });
+
     return countName
-  }
-  var NamesCats = nameStats(list) //счет имен котов
+}
+var NamesCats = nameStats(list) //счет имен котов
